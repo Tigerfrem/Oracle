4a5dadb2613bd22c78edbb789b903b979e9f63e3
Tiger <llamperouge265@gmail.com>
2022-07-05 20:21:09 -0700

    Added more functions & fixed issues

--- Discord_Bot/main.js
+++ Discord_Bot/main.js
@@ -1,21 +1,35 @@
 const Discord = require('discord.js');
 
-const client = new Discord.Client({ intents: []});
+const client = new Discord.Client({ intents: ['DIRECT_MESSAGES', 'GUILD_MESSAGES', 'GUILDS'],
+partials: ['MESSAGE', 'CHANNEL'] });
 
-const prefix = '-';
+const prefix = '>'; 
 
 client.once('ready', () => {
     console.log('Oracle is online!');
 });
 
 client.on('message', message =>{ 
-    if(!message.content.startswith(prefix) || message.author.bot) return;
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+    
+    //console.log('Sending message...');
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
 
-    if(command === 'ping'){
-        message.channel.send('pong!');
+    if(message.content.startsWith(prefix)){
+        const args = message.content.substring(prefix.length)
+    
+        const command = args.toLowerCase();
+        //message.channel.send('Did you type '+command+'?');
+        console.log(command);
+
+        if(command === 'ping'){
+            message.channel.send('pong!');
+        }else if(command === "youtube"){
+            message.channel.send('https://www.youtube.com')
+        }else if(command === "eminem"){
+            message.channel.send('https://www.youtube.com/eminem')
+        }
+
     }
 });
 
